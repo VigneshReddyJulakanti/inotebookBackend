85614c27731a2b2bcaccdeb9d8fa4be981835313
VigneshReddyJulakanti <73946941+VigneshReddyJulakanti@users.noreply.github.com>
2022-01-10 15:34:20 +0530

    create mongoose db played with req , res

--- routes/auth.js
+++ routes/auth.js
@@ -2,11 +2,65 @@ const express =require('express');
 const router=express.Router()
 const User = require("../models/User")
 
-router.get('/',(req,res)=>{
-        console.log(req.body);
-        const user =User(req.body);
-        user.save();
-        res.send(req.body); 
+
+const mongoose=require("mongoose")
+const { Schema } = mongoose;
+
+const { body, validationResult } = require('express-validator');
+
+router.post('/createUser',[
+
+  
+        
+    body('email',"Invalid mail").isEmail(),
+    body('password',"Invalid pass").isLength({ min: 5 }),
+    body('name',"Invalid name").isLength({ min: 3})
+
+   
+
+
+        ],async (req,res)=>{
+
+
+            
+
+            
+
+            const errors = validationResult(req);
+            
+            if (!errors.isEmpty()) {
+              return res.status(400).json({ errors: errors.array() });
+            }
+            try{
+
+            // user = await User.create({
+            //     name: req.body.name,
+            //     password: req.body.password,
+            //     email: req.body.email
+            //   });
+            // res. json(user)
+            const b=new Schema(
+            {hai:{
+                type : Number
+            }}
+            )
+
+            const d=mongoose.model("b",b);
+            c=await d.create({hai:5});
+
+              res.json(c);
+           
+          
+
+        }catch(error){
+            console.log(error)
+            return res.status(400).json(error);
+            // return res.status(400).json({"errr":"Some error occured"});
+        }
+
+            
+              
+
 })
 router.post('/',(req,res)=>{
     obj={
